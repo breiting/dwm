b34c22b2c5dbed5f415a8f35930206d0a1294ea7
Bernhard Reitinger <br@rexos.org>
2020-04-01 17:14:57 +0200

    update config

--- config.h
+++ config.h
@@ -66,10 +66,9 @@ static const Layout layouts[] =
 static char dmenumon[2]             = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]       = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *dmenuunicode[]   = { "/home/breiting/.bin/dmenuunicode", NULL };
-static const char *termcmd[]        = { "st", NULL };
-static const char *rangercmd[]      = { "st", "-e", "ranger" };
-static const char *vifmcmd[]        = { "st", "-e", "vifm" };
-static const char *muttcmd[]        = { "st", "-e", "neomutt" };
+static const char *termcmd[]        = { "alacritty", NULL };
+static const char *rangercmd[]      = { "alacritty", "-e", "ranger" };
+static const char *muttcmd[]        = { "alacritty", "-e", "neomutt" };
 static const char *cmdlock[]        = { "slock", NULL };
 static const char *cmdclipmenu[]    = { "clipmenu", NULL };
 static const char *cmdenv[]         = { "environment", NULL };
@@ -105,7 +104,6 @@ static Key keys[] =
     { MODKEY | ShiftMask,           XK_m,                      setlayout,      {.v = &layouts[2]} },
     { MODKEY,                       XK_m,                      spawn,          {.v = muttcmd } },
     { MODKEY,                       XK_r,                      spawn,          {.v = rangercmd } },
-    { MODKEY,                       XK_f,                      spawn,          {.v = vifmcmd } },
     { MODKEY | ShiftMask,           XK_p,                      togglefloating, {0} },
     { MODKEY,                       XK_0,                      view,           {.ui = ~0 } },
     { MODKEY | ShiftMask,           XK_0,                      tag,            {.ui = ~0 } },
@@ -122,9 +120,8 @@ static Key keys[] =
     TAGKEYS (XK_7,                      6)
     TAGKEYS (XK_8,                      7)
     TAGKEYS (XK_9,                      8)
-    {
-        MODKEY | ShiftMask,         XK_q,      quit,           {0}
-    },
+    { MODKEY | ShiftMask,         XK_q,      quit,           {0} },
+    { MODKEY | ShiftMask,         XK_r,      quit,           {1} },
 };
 
 /* button definitions */
